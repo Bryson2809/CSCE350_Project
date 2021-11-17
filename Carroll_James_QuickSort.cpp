eb4c40cc39aa8beb429bad511c9b5a42432a16fe
bryson2809 <jbc2@email.sc.edu>
2021-11-17 10:48:57 -0500

    Progress?

--- Carroll_James_QuickSort.cpp
+++ Carroll_James_QuickSort.cpp
@@ -7,6 +7,47 @@
 
 using namespace std;
 
+const int MAX_VALUES = 1000;
+
+class HelperFunctions 
+{
+private:
+	int medianOfThree(int first, int middle, int last, array<float, MAX_VALUES> arr)
+	{
+		if ((arr[first] < arr[last] && arr[first] > arr[middle]) || (arr[first] > arr[last] && arr[first] < arr[middle]))
+			return first;
+		else if ((arr[middle] < arr[last] && arr[middle] > arr[first]) || (arr[middle] < arr[first] && arr[middle] > arr[last]))
+			return middle;
+		else
+			return last;
+	}
+
+public:
+	HelperFunctions()
+	{
+
+	}
+
+	~HelperFunctions()
+	{
+
+	}
+
+	void sort_array(array<float, MAX_VALUES> arr)
+	{
+		int lastElement = 0;
+
+		while (arr[lastElement + 1] != NULL)
+		{
+			lastElement++;
+			//cout << arr[lastElement] << endl;
+		}
+		int test = medianOfThree(0, arr.size() / 2, lastElement, arr);
+
+		//cout << test << endl;
+	}
+};
+
 int main(int argc, char* argv[])
 {
 
@@ -23,14 +64,23 @@ int main(int argc, char* argv[])
 		return 1;
 	}
 
-	array<float, 5> values;
+	//float values[MAX_VALUES];
 
-	for (int i = 0; i < 5; i++)
+	array<float, MAX_VALUES> values;
+
+	for (int i = 0; input >> values[i]; i++);
+
+	cout << "Should be 3 4 8 5 1 44 56 73 69 420 32 12" << endl;
+
+	for (int i = 0; values[i + 1] != NULL; i++)
 	{
-		input >> values[i];
+		cout << values[i] << endl;
 	}
 
 	input.close();
+
+	HelperFunctions helper;
+	helper.sort_array(values);
 
 	ofstream output(argv[2]);
 
@@ -40,7 +90,7 @@ int main(int argc, char* argv[])
 		return 1;
 	}
 
-	for (int i = 0; i < 5; i++)
+	for (int i = 0; i < 12; i++)
 	{
 		output << values[i] << " ";
 	}
