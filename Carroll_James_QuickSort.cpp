51d4cc159db109e567bf56b1cfbaa7729440024f
bryson2809 <jbc2@email.sc.edu>
2021-11-18 20:04:47 -0500

    Changed to vectors

--- Carroll_James_QuickSort.cpp
+++ Carroll_James_QuickSort.cpp
@@ -3,51 +3,53 @@
 #include <fstream>
 #include <sstream>
 #include <cstdlib>
-#include <array>
+#include <vector>
 
 using namespace std;
 
-const int MAX_VALUES = 100000;
+const int MAX_VALUE = 100000;
 
 class HelperFunctions 
 {
 private:
-	static int medianOfThree(array<float, MAX_VALUES> arr)
+	static int medianOfThree(vector<float> v)
 	{
 		int first = 0;
-		int last = 0;
+		int last = v.size() - 1;
+		int middle = v[v.size() / 2];
 		int pivot = 0;
-		for (int i = 0; arr[i] != NULL; i++)
-			last++;
-		int middle = last / 2;
 
-		pivot = HelperFunctions::findPivot(arr[first], arr[middle], arr[last]);
+		if (v[first] > v[middle])
+			HelperFunctions::swap(first, middle, v);
+		if (v[middle] > v[last])
+			HelperFunctions::swap(middle, last, v);
+		if (v[first] > v[last])
+			HelperFunctions::swap(first, last, v);
 
-		if (arr[first] > arr[middle])
-			HelperFunctions::swap(first, middle, arr);
-		if (arr[middle] > arr[last])
-			HelperFunctions::swap(middle, last, arr);
-		if (arr[first] > arr[last])
-			HelperFunctions::swap(first, last, arr);
+		return pivot;
 	}
 
-	static void swap(int leftIndex, int rightIndex, array<float, MAX_VALUES>& arr)
+	static void swap(int leftIndex, int rightIndex, vector<float>& v)
 	{
 		float temp = 0.0;
-		temp = arr[leftIndex];
-		arr[leftIndex] = arr[rightIndex];
-		arr[rightIndex] = temp;
+		temp = v[leftIndex];
+		v[leftIndex] = v[rightIndex];
+		v[rightIndex] = temp;
 	}
 
-	static int findPivot(float first, float middle, float last)
+	/*static int findPivot(vector<float>& v)
 	{
+		float first = v[0];
+		float middle = v[v.size() / 2];
+		float last = v.size() - 1;
+		
 		if ((first < middle && middle < last) || (first > middle&& middle > last))
 			return middle;
 		else if ((middle > first&& first > last) || (middle < first && first < last))
 			return first;
 		else
 			return last;
-	}
+	}*/
 
 public:
 	HelperFunctions()
@@ -60,19 +62,24 @@ public:
 
 	}
 
-	static void sort_array(array<float, MAX_VALUES>& arr)
+	/*static void sort_Array(vector<float>& v)
 	{
 		float temp = 0.0;
-		for (int i = 0; arr[i] != NULL; i++)
+		for (int i = 0; i < v.size(); i++)
 		{
-			for (int j = i; arr[j] != NULL; j++)
+			for (int j = i; j < v.size(); j++)
 			{
-				if (arr[i] > arr[j])
+				if (v[i] > v[j])
 				{
-					HelperFunctions::swap(i, j, arr);
+					HelperFunctions::swap(i, j, v);
 				}
 			}
 		}
+	}*/
+
+	public void quick_sort(vector<float>& v, int part)
+	{
+
 	}
 };
 
@@ -92,13 +99,27 @@ int main(int argc, char* argv[])
 		return 1;
 	}
 
-	array<float, MAX_VALUES> values;
+	float *temp = new float[MAX_VALUE];
+	int size = 0;
 
-	for (int i = 0; input >> values[i]; i++);
+	for (int i = 0; input >> temp[i]; i++)
+		size++;
+
+	vector<float> values (size);
+
+	for (int i = 0; i < size; i++)
+	{
+		values[i] = temp[i];
+	}
+
+	delete[] temp;
 
 	input.close();
-	
-	HelperFunctions::sort_array(values);
+
+	for (int i = 0; i < size; i++)
+		cout << values[i] << endl;
+
+	HelperFunctions::sort_Array(values);
 
 	ofstream output(argv[2]);
 
@@ -108,7 +129,7 @@ int main(int argc, char* argv[])
 		return 1;
 	}
 
-	for (int i = 0; values[i] != NULL; i++)
+	for (int i = 0; i < size; i++)
 		output << values[i] << endl;
 	output.close();
 	return 0;
